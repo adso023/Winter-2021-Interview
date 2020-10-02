e0b43be9859347777a23a1693877cbf143b36e89
adso023 <43103279+adso023@users.noreply.github.com>
2020-10-01 21:08:14 -0400

    For the branch

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
-import express from 'express';
-import { IPokemon, Pokemon } from './pokemon/pokemon';
-import url from 'url';
+import express from "express";
+import { IPokemon, Pokemon } from "./pokemon/pokemon";
+import url from "url";
 
 const app = express();
 app.use(express.json());
 const port = 8001; // default port to listen
 
 // define a route handler for the default home page
-app.get('/', async (request: any, response: any) => {
+app.get("/", async (request: any, response: any) => {
   response.send({});
 });
 
 // Test the pokemon endpoint
-app.get('/pokemon', async (request, response) => {
+app.get("/pokemon", async (request, response) => {
   const p = new Pokemon();
   // Sends in the requested name
   try {
     const result = await p.getPokemonByName(String(request.query.name));
+    console.log(result);
     // const names: string[] = request.query.name.toLocaleString().split(",");
     // names.forEach((name) => console.log(`Name: ${name}`));
     // const result = await p.getPokemonsByNameList(names);
@@ -39,7 +40,7 @@ app.get('/pokemon', async (request, response) => {
     console.log(e);
     response.status(500);
     response.send({
-      error: 'Could not find pokemon',
+      error: "Could not find pokemon",
     });
   }
 });
